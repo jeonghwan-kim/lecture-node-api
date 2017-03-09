dd157391c3cd8d76868aeaf8c77d263970c8fccb
김정환 <6pack@woowahan.com>
2017-03-10 00:38:14 +0900

    첫 api만들기

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var app = express();
+var morgan = require('morgan');
+var users = [
+  {id: 1, name: 'alice'},
+  {id: 2, name: 'bek'},
+  {id: 3, name: 'chris'}
+];
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.use(morgan('dev'));
+
+app.get('/users', function (req, res) {
+  res.json(users);
 });
 
 app.listen(3000, function () {
