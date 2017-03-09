3e8584ef6c1121891114aca43e242794a3eeda72
김정환 <6pack@woowahan.com>
2017-03-09 21:12:14 +0900

    라우팅 추가

--- index.js
+++ index.js
@@ -4,9 +4,21 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+
+  if (req.url === '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World\n');
+  } else if (req.url === '/users') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('User list');
+  } else {
+    res.statusCode = 404;
+    res.end('Not Found');
+  }
+
+  
 });
 
 server.listen(port, hostname, () => {
--- math.js
+++
@@ -1,7 +0,0 @@
-function sum (a, b) {
-  return a + b;
-}
-
-module.exports = {
-  sum: sum
-};
