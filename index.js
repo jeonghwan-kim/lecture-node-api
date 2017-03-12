f31692c86637e4c6dffaabdf2688c76fabf7c464
김정환 <6pack@woowahan.com>
2017-03-12 11:43:21 +0900

    api 개발

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var morgan = require('morgan');
+var bodyParser = require('body-parser');
 var users = [
   {id: 1, name: 'alice'},
   {id: 2, name: 'bek'},
@@ -8,6 +9,9 @@ var users = [
 ];
 
 app.use(morgan('dev'));
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
 
 app.get('/users', function (req, res) {
   req.query.limit = req.query.limit || 10;
@@ -17,6 +21,57 @@ app.get('/users', function (req, res) {
   }
   res.json(users.slice(0, limit));
 });
+
+app.get('/users/:id', function(req, res) {
+  const id = parseInt(req.params.id, 10);
+  if (Number.isNaN(id)) return res.status(400).end();
+
+  const user = users.filter((user) => user.id === id)[0];
+  if (!user) return res.status(404).end();
+
+  res.json(user);
+});
+
+app.delete('/users/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  if (Number.isNaN(id)) return res.status(400).end();
+
+  users = users.filter(user=> user.id !== id);
+  res.status(204).end();
+});
+
+app.post('/users', (req, res) =>{
+  const name = req.body.name;
+  if (!name) return res.status(400).end();
+
+  const isConflict = users.filter(user => user.name === name).length;
+  if (isConflict) return res.status(409).end();
+
+  const id = Date.now();
+  const user = {id, name};
+  users.push(user);
+  res.status(201).json(user);
+});
+
+app.put('/users/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  if (Number.isNaN(id)) return res.status(400).end();
+
+  const name = req.body.name;
+  if (!name) return res.status(400).end();
+
+  const isConflict = users.filter(user => user.name === name).length;
+  if (isConflict) return res.status(409).end();
+
+  const user = users.filter(user=> user.id === id)[0];
+  if (!user) return res.status(404).end();
+
+  user.name = name;
+
+  res.json(user);
+});
+
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
--- index.spec.js
+++ index.spec.js
@@ -31,3 +31,128 @@ describe('GET /users는', ()=> {
     })
   })
 });
+describe('GET /users/:id는', ()=> {
+  describe('성공시', ()=> {
+    it('id가 1인 유저 객체를 반환한다', (done) => {
+      request(app)
+          .get('/users/1')
+          .end((err, res) =>{
+            res.body.should.have.property('id', 1);
+            done();
+          });
+    });
+  });
+  describe('실패시', ()=> {
+    it('id가 숫자가 아닐경우 400으로 응답한다', (done) => {
+      request(app)
+          .get('/users/one')
+          .expect(400)
+          .end(done);
+    });
+    it('id로 유저를 찾을수 없을 경우 404로 응답한다', (done)=>{
+      request(app)
+          .get('/users/999')
+          .expect(404)
+          .end(done);
+    });
+  })
+});
+describe('DELETE /users/:id', ()=> {
+  describe('성공시', ()=> {
+    it('204를 응답한다', (done)=>{
+      request(app)
+          .delete('/users/1')
+          .expect(204)
+          .end(done);
+    })
+  });
+  describe('실패시', ()=> {
+    it('id가 숫자가 아닐경우 400으로 응답한다', done=>{
+      request(app)
+          .delete('/users/one')
+          .expect(400)
+          .end(done);
+    });
+  });
+});
+describe('POST /users', ()=>{
+  describe('성공시', ()=> {
+    let name = 'daniel',
+        body;
+    before(done=>{
+      request(app)
+          .post('/users')
+          .send({name})
+          .expect(201)
+          .end((err, res) => {
+            body = res.body;
+            done();
+          });
+    });
+    it('생성된 유저 객체를 반환한다', ()=>{
+      body.should.have.property('id');
+    });
+    it('입력한 name을 반환한다', ()=> {
+      body.should.have.property('name', name)
+    })
+  });
+  describe('실패시', ()=> {
+    it('name 파라매터 누락시 400을 반환한다', (done)=>{
+      request(app)
+          .post('/users')
+          .send({})
+          .expect(400)
+          .end(done)
+    });
+    it("name이 중복일 경우 409를 반환한다", done=>{
+      request(app)
+          .post('/users')
+          .send({name: 'daniel'})
+          .expect(409)
+          .end(done)
+    })
+  })
+});
+describe('PUT /users/:id', ()=>{
+  describe('성공시', ()=> {
+    it('변경된 name을 응답한다', (done)=> {
+      const name = 'chally';
+      request(app)
+          .put('/users/3')
+          .send({name})
+          .end((err, res) =>{
+            res.body.should.have.property('name', name);
+            done();
+          });
+    })
+  });
+  describe('실패시', ()=>{
+    it('정수가 아닌 id일 경우 400을 응답한다', done=>{
+      request(app)
+          .put('/users/one')
+          .expect(400)
+          .end(done);
+    });
+    it('name이 없을 경우 400을 응답한다', done=>{
+      request(app)
+          .put('/users/1')
+          .send({})
+          .expect(400)
+          .end(done);
+    });
+    it('없는 유저일 경우 404을 응답한다', done=>{
+      request(app)
+          .put('/users/999')
+          .send({name: 'foo'})
+          .expect(404)
+          .end(done);
+    });
+    it('이름이 중복일 경우 409을 응답한다', done=>{
+      request(app)
+          .put('/users/3')
+          .send({name: 'bek'})
+          .expect(409)
+          .end(done);
+    })
+  })
+})
