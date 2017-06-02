3f7db87e1e1a1157395fe269f05890065165a1ba
김정환 <ej88ej@gmail.com>
2017-06-02 09:28:54 +0900

    Update sync-db

--- bin/sync-db.js
+++ bin/sync-db.js
@@ -1,5 +1,8 @@
 const models = require('../models');
 
 module.exports = () => {
-  return models.sequelize.sync({force: true});
+  const options = {
+    force: process.env.NODE_ENV === 'test' ? true : false
+  };
+  return models.sequelize.sync(options);
 }
